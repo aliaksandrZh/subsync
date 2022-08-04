cd9000df9346ee4b90962ca31d0822ed077aa4d0
aliaksandr <aliaksandr.zhebit@gmail.com>
2022-08-04 16:58:13 +0300

    subsync

---
+++ index.ts
@@ -0,0 +1,21 @@
+interface Unsubscribable {
+  unsubscribe(): void;
+}
+
+const isFunction = (fn: unknown) => typeof fn === 'function';
+
+export class Subsync {
+  private _subs: Unsubscribable[] = [];
+
+  set sync(subscription: Unsubscribable) {
+    if (!isFunction(subscription.unsubscribe)) {
+      throw Error('.unsubscribe must be a function');
+    }
+
+    this._subs.push(subscription);
+  }
+
+  public unsubscribe(): void {
+    this._subs.forEach(sub => sub.unsubscribe())
+  }
+}
